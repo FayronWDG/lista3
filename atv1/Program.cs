840fed250979ade1a55d34950c336f0919b9b6a6
FayronWDG <fayronwdg@gmail.com>
2025-04-23 19:10:04 +0000

    aq é nois

---
+++ atv1/Program.cs
@@ -0,0 +1,14 @@
+﻿//definindo variaveis
+int contador=0,resultado=0;
+
+//fazendo a conta e usando a repetição para testar os números necessarios
+for(int i=1;i<=500;i++){
+    if((i%2)!=0 && (i%3)==0){
+        resultado=resultado+i;
+        contador++;
+    }
+}
+//mostrando na tela do usuario.
+Console.WriteLine($"\nExistem {contador} números impares multiplos de 3 entre 1 e 500 \ne a soma desses números resulta em: {resultado}.");
+//pra dar tempo da pessoa ver sem o programa fechar na cara dela.
+Console.ReadKey();
