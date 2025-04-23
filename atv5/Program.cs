42bfc6fdb593f68c1895a2f1b8a681fd4f495986
FayronWDG <fayronwdg@gmail.com>
2025-04-23 23:54:26 +0000

    kkkkk

--- atv5/Program.cs
+++ atv5/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int decimais=0
+double pi=0,termo=0;
+
+Console.WriteLine("\n\nCalculando aproximação de PI\n\n");
+
+Console.WriteLine("");
+
+for (int i = 0; i < decimais; i++)
+{
+    // Cálculo do termo da série
+    termo=Math.Pow(-1, i) / (2 * i + 1);
+    // Soma o termo à aproximação de pi
+    pi=pi+termo;
+}
+
+        
+pi=pi*4;
+
+Console.WriteLine($"Aproximação de pi usando a série de Leibniz: {pi}");
